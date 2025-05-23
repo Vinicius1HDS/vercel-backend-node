d37055196c08a1691e9cbb622dbc2fe03bb8f5f7
Vinicius1HDS <vinicius.hdasilva1@gmail.com>
2025-05-23 16:32:17 -0300

    conection with railway db

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: 'sql207.infinityfree.com',
-  user: 'if0_39053680_finances_db',
-  password: 'if0_39053680',
-  database: 'o0dZovWJQpt',
+  host: 'mainline.proxy.rlwy.net',
+  user: 'root',
+  password: 'VSfhHncvcdMXPrqjVPZrLPbUpavJlhEU', 
+  database: 'railway',
+  port: 41081,
 });
