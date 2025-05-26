5989d4bb8f049293601cfc4af052b6c520827318
Vinicius1HDS <vinicius.hdasilva1@gmail.com>
2025-05-26 15:36:41 -0300

    fix: double quotes causing CORS missing allow origin

--- api/create_recurring_bills.js
+++ api/create_recurring_bills.js
@@ -1,12 +1,12 @@
 import { pool } from '../db.js';
 
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.methosd === "OPTIONS") return res.status(204).end();
-    if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed."});
+    if (req.methosd === 'OPTIONS') return res.status(204).end();
+    if (req.method !== 'POST') return res.status(405).json({ error: "Method not allowed."});
 
     try {
         const chunks = [];
